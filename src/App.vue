b646e3b75da54a3466f959cbb878c69a834ce575
Fatymaah <fatymahmohamed@gmail.com>
2018-07-11 15:06:02 +0300

    Initial repo

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <h3>{{ title }}</h3>
+    <form>
+    <textarea placeholder="Add new Task" v-model="newTask"></textarea>
+    <p>{{newTask}}</p>
+    <button type="button" v-on:click="addTask" > Add Task {{tasks.length}} </button>
+    </form>
+
+    <ul>
+    <li v-for="tasks in tasks"> {{tasks}} 
+
+    </li>
+    </ul>
+    
+
+
+   
+    
+
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      title: 'My First App',
+      click: 0 ,
+      newTask:'SAMPLE',
+      
+      tasks:[
+      
+      ]
+    }
+  },
+  methods:{
+    addTask(){
+      this.tasks.unshift(this.newTask);
+      this.newTask = '';
+    
+    }
+  }
+}
+</script>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import 'milligram/dist/milligram.css'
+import './app.css'
+import Axios from 'axios'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
